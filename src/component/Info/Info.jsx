1c43bf670778fd63915d4ab4ec014bf601cfdcbd
Fredi Anriko <fredianriko@gmail.com>
2022-07-30 21:42:39 +0700

    fix name style

--- src/component/Info/Info.jsx
+++ src/component/Info/Info.jsx
@@ -6,7 +6,14 @@ function Info({data, dataNow}) {
   console.log('DATA', data)
 
   const pokemonImage = dataNow ? dataNow?.sprites.front_default : data?.sprites.front_default
-  const pokemonName = dataNow ? dataNow?.name : data?.name
+  const pokemonName = dataNow ? dataNow?.name.toUpperCase() : data?.name.toUpperCase()
+  const pokemonHp = dataNow ? dataNow?.stats[0].base_stat : data?.stats[0].base_stat
+  const pokemonAttack = dataNow ? dataNow?.stats[1].base_stat : data?.stats[2].base_stat
+  const pokemonDefense = dataNow ? dataNow?.stats[2].base_stat : data?.stats[2].base_stat
+  const pokemonSpecialAttack = dataNow ? dataNow?.stats[3].base_stat : data?.stats[3].base_stat
+  const pokemonSpecialDefense = dataNow ? dataNow?.stats[4].base_stat : data?.stats[4].base_stat
+  const pokemonSpeed = dataNow ? dataNow?.stats[5].base_stat : data?.stats[5].base_stat
+
 
   return (
 
@@ -16,10 +23,15 @@ function Info({data, dataNow}) {
                <img src={pokemonImage} alt="" />
              </div>
              <div className="name">
-                <h1>Name : {pokemonName}</h1>
+                <h2>{pokemonName}</h2>
              </div>
              <div className="basic-stats">
-                <h2></h2>
+                <h2>Hp <span style={{marginLeft:'135px'}}>:</span> {pokemonHp} </h2>
+                <h2>Attack <span style={{marginLeft: '99px'}}>:</span> {pokemonAttack}</h2>
+                <h2>Defense <span style={{marginLeft: '89px'}}>:</span> {pokemonDefense}</h2>
+                <h2>Special-Attack <span style={{marginLeft: '19px'}}>:</span> {pokemonSpecialAttack}</h2>
+                <h2>Special-Defense <span style={{marginLeft: '9px'}}>:</span> {pokemonSpecialDefense}</h2>
+                <h2>Speed<span style={{marginLeft: '116px'}}>:</span> {pokemonSpeed}</h2>
              </div>
          </div>      
      </div>
